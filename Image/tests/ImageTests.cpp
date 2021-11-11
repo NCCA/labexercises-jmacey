f697069a1b865f993ed688ae6b7497cf813c8961
jmacey <jmacey@bournemouth.ac.uk>
2021-11-11 18:58:26 +0000

    added initial DLA from lab session

--- Image/tests/ImageTests.cpp
+++ Image/tests/ImageTests.cpp
@@ -1,6 +1,6 @@
 #include "Image.h"
 #include <gtest/gtest.h>
-
+#include <fmt/format.h>
 TEST(RGBA,ctor)
 {
   RGBA a;
@@ -30,7 +30,84 @@ TEST(IMAGE,ctor)
 
 TEST(IMAGE,userCtor)
 {
-  Image img(50,50);
+  Image img(250,150);
+  ASSERT_EQ(img.width(),250);
+  ASSERT_EQ(img.height(),150);
+  for(size_t y=0; y<img.height(); ++y)
+  {
+    for(size_t x=0; x<img.width(); ++x)
+    {
+      auto pixel=img.getPixel(x,y);
+      ASSERT_EQ(pixel.r,0);
+      ASSERT_EQ(pixel.g,0);
+      ASSERT_EQ(pixel.b,0);
+      ASSERT_EQ(pixel.a,255);
+      
+    }
+  }
 }
+
+TEST(IMAGE,userCtorColour)
+{
+  Image img(250,150,255,0,0,255);
+  ASSERT_EQ(img.width(),250);
+  ASSERT_EQ(img.height(),150);
+  for(size_t y=0; y<img.height(); ++y)
+  {
+    for(size_t x=0; x<img.width(); ++x)
+    {
+      auto pixel=img.getPixel(x,y);
+      ASSERT_EQ(pixel.r,255);
+      ASSERT_EQ(pixel.g,0);
+      ASSERT_EQ(pixel.b,0);
+      ASSERT_EQ(pixel.a,255);
+      
+    }
+  }
+
+
+}
+
+
+TEST(IMAGE,writeTofile)
+{
+  Image img(1024,720,255,0,0,255);
+  ASSERT_TRUE(img.save("test.exr"));
+}
+
+TEST(IMAGE,setPixel)
+{
+  Image img(100,100,255,255,255,255);
+  for(int i=0; i<img.width(); ++i)
+  {
+    img.setPixel(i,30,{255,0,0,255});
+    img.setPixel(i,31,{255,0,0,255});
+    img.setPixel(i,32,{255,0,0,255});
+    img.setPixel(i,33,{255,0,0,255});
+    img.save(fmt::format("band.{}.exr",i));
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
